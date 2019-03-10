d6703f6fe5aa3e0a40550a085b7b79c81d0b4938
tushar goel <tushar.goel.dav@gmail.com>
2019-03-10 14:17:48 +0530

    feed added
    Signed-off-by: tushar goel <tushar.goel.dav@gmail.com>

--- index.js
+++ index.js
@@ -1,28 +1,68 @@
 const {MongoClient}=require('mongodb')
 const {MongoCron}= require('mongodb-cron');
 const Mongourl= 'mongodb://localhost:27017';
+const express=require('express')
+const app=express();
+const http=require('http');
+const server=http.createServer(app);
+const socketio=require('socket.io');
+const io=socketio(server);
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(__dirname+'/public'));
+app.get('/',(req,res)=>{
+  res.sendFile('index.html')
+})
+app.get('/feed',(req,res)=>{
+  MongoClient.connect(Mongourl,{useNewUrlParser:true}).then((mongo)=>{
+    const db= mongo.db('scheduler');
+    const posts=db.collection('posts');
+    posts.find({}).toArray((err,resp)=>{
+      res.send(resp)
+    })
+  })
+})
+app.post('/schedulepost',(req,res)=>{
 (async()=>{
 const mongo = await MongoClient.connect(Mongourl,{useNewUrlParser:true});
 const db= mongo.db('scheduler');
 const collection = db.collection('jobs');
-const posts=db.collection('posts');
-const job = await collection.insert({
-    sleepUntil: new Date('2019-03-07,11:57'),
-    content: 'This is awesome',
-    createdBy : 'Ram',
-    onDate: new Date()
-  });
-const cron = new MongoCron({
-    collection, // a collection where jobs are stored
-    onStart: async () => console.log('running'),
-    onDocument: async (doc) => {
-        posts.insertOne({
-            postcontent:doc.content,
-            creator:doc.createdBy
-        })
-    }, // triggered on job processing
-    onError: async (err) => console.log(err), // triggered on error
-  });
-   
-  cron.start();
+const job = await collection.insertOne({
+  sleepUntil: new Date(req.body.date_time),
+  autoRemove : true,
+  content: req.body.content,
+  createdBy : req.body.username,
+  onDate: new Date()
+});
 })();
+})
+
+MongoClient.connect(Mongourl,{useNewUrlParser:true}).then((mongo)=>{
+  const db= mongo.db('scheduler');
+  const collection = db.collection('jobs');
+  const posts=db.collection('posts');
+  const cron = new MongoCron({
+      collection, // a collection where jobs are stored
+      onStart: async () => console.log('running'),
+      onDocument: async (doc) => {
+        console.log('inserted sucessfully')
+          posts.insertOne({
+              postcontent:doc.content,
+              creator:doc.createdBy
+          })
+      }, // triggered on job processing
+      onError: async (err) => console.log(err), // triggered on error
+    });
+    cron.start();
+})
+io.on('connection',(socket)=>{
+  socket.emit('connected')
+  MongoClient.connect(Mongourl,{useNewUrlParser:true}).then((mongo)=>{
+    const db= mongo.db('scheduler');
+    const posts=db.collection('posts');
+    posts.find({}).toArray((err,resp)=>{
+      socket.emit('feed',resp)
+    })
+  })
+})
+server.listen(2000)
